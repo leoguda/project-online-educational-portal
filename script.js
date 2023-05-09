bdc2ef4578fdf36f36b35f57c9e2e79167f006ca
leoguda <pitodipli002@gmail.com>
2023-05-09 16:25:12 +0400

    working on header
    added part of responsive.
    now code for burger menu and toggleable search bar is left to be written.

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
-let menu = document.querySelector("#categories");
-let menuDiv = document.querySelector("#categories-div");
-let menuDiv1 = document.querySelector("#categories-div div");
-
-let links = document.getElementsByClassName(".categories-links");
-let links2 = document.getElementsByClassName(".categories-links2");
-
-
-menu.onmouseover = function() {mouseOver()};
-menu.onmouseout = function() {mouseOut()};
-menuDiv.onmouseover = function() {mouseOver()};
-menuDiv.onmouseout = function() {mouseOut()};
-menuDiv1.onmouseover = function() {mouseOver()};
-menuDiv1.onmouseout = function() {mouseOut()};
-
-// links.onclick = function() {mouseOver1()}
-links.onmouseout = function() {mouseOut1()}
+// start - nav
+let menuCategories = document.querySelector("#categories"); // variable of span with text "categories"
+let menuDiv = document.querySelector("#categories-div"); // variable of container of categories
+let menuDivDivs = document.getElementsByClassName("categories-div2"); // variable of containers of links of categories
 
 function mouseOver() {
     menuDiv.style.display = "flex";
-    menu.style.padding = "3em 0";
-}
-
+    menuCategories.style.padding = "3em 0";
+    menuCategories.style.cursor = "pointer";
+}    // this function adds content of categories
 function mouseOut() {
     menuDiv.style.display = "none";
-    menu.style.padding = "0";
-}
-
+    menuCategories.style.padding = "0";
+}   // this function removes content of categories
 function mouseOver1(linkName){
-    
+    for (div of menuDivDivs){
+        div.classList.remove ("active-links2");
+    }
     document.getElementById(linkName).classList.add("active-links2");
-}
+}   // this function adds and removes child content of categories
+// end - nav
 
-function mouseOut1(){
-    // for (link of links2){
-    //     link.classList.remove ("active-links2");
-    // }
-    document.getElementById(linkName).classList.remove("active-links2");
 
-}
+
